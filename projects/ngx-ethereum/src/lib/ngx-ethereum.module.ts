50d40634403816c103f29fc7acd764529416ed2e
Rahma BEL HADJ <rahma.belhadj@neofacto.com>
2022-08-22 16:59:57 +0200

    Add the AccountComponent

--- projects/ngx-ethereum/src/lib/ngx-ethereum.module.ts
+++ projects/ngx-ethereum/src/lib/ngx-ethereum.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { AccountComponent } from './account/account.component';
 import { ShortAddressPipe } from './pipes/short-address.pipe';
 
 @NgModule({
-  declarations: [ShortAddressPipe],
+  declarations: [ShortAddressPipe, AccountComponent],
   imports: [],
   exports: [],
 })
